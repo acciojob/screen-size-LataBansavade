7b1d61e566cf0c19ca4824a5776daa2c4f0c9059
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-09-24 16:59:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateWindowSize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  console.log(`Window Width: ${width}px, Window Height: ${height}px`);
+}
+
+// Initial window size
+updateWindowSize();
+
+// Add an event listener for the "resize" event
+window.addEventListener('resize', updateWindowSize);
