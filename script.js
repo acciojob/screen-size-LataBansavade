ddd987573599256f8be64f3ed0d2d9e1eab9acef
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-09-29 12:43:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 //your JS code here. If required.
+// Function to update the width and height of the window
+const div = document.getElementById("sizeInfo");
+let tag = document.createElement("h1");
 function updateWindowSize() {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  console.log(`Window Width: ${width}px, Window Height: ${height}px`);
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  // Display the width and height in the console
+	tag.innerHTML =`Width: ${width} and Height: ${height}`
+  
 }
-
-// Initial window size
-updateWindowSize();
-
-// Add an event listener for the "resize" event
+div.appendChild(tag);
+// Add an event listener for the window's resize event
 window.addEventListener('resize', updateWindowSize);
+
+// Call the function initially to get the initial dimensions
+updateWindowSize();
